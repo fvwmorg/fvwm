84696ad2c947db0afdd879e4b45ee1846b0976c9
migo <migo>
2002-02-27 18:01:08 +0000

    * Backported conditional command This. _ This is equivalent to Silent WindowId $w

--- fvwm/conditional.c
+++ fvwm/conditional.c
@@ -783,6 +783,37 @@ void CMD_Pick(F_CMD_ARGS)
   }
 }
 
+void CMD_This(F_CMD_ARGS)
+{
+	char *restofline;
+	char *flags;
+	WindowConditionMask mask;
+
+	if (!tmp_win)
+	{
+		return;
+	}
+
+	flags = CreateFlagString(action, &restofline);
+	DefaultConditionMask(&mask);
+	mask.my_flags.use_circulate_hit = 1;
+	mask.my_flags.use_circulate_hit_icon = 1;
+	CreateConditionMask(flags, &mask);
+	if (flags)
+	{
+		free(flags);
+	}
+
+	if (MatchesConditionMask(tmp_win, &mask) && restofline)
+	{
+		old_execute_function(
+			restofline, tmp_win, eventp, C_WINDOW, *Module, 0, NULL);
+	}
+
+	FreeConditionMask(&mask);
+	return;
+}
+
 void CMD_WindowId(F_CMD_ARGS)
 {
   FvwmWindow *t;
