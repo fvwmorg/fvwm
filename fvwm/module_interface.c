e9608cd57a4fd0f2d2c00f9b7e31583dd02873e0
renato <renato>
2006-12-30 20:09:31 +0000

    Fixed an error on the previous patch (its "module_free()" and not "free_module()")

--- fvwm/module_interface.c
+++ fvwm/module_interface.c
@@ -320,7 +320,7 @@ static fmodule *do_execute_module(
 	if (action == NULL)
 	{
 		free(args);
-		free_module(module);
+		module_free(module);
 
 		return NULL;
 	}
@@ -338,7 +338,7 @@ static fmodule *do_execute_module(
 	if (!cptr)
 	{
 		free(args);
-		free_module(module);
+		module_free(module);
 
 		return NULL;
 	}
@@ -360,7 +360,7 @@ static fmodule *do_execute_module(
 		}
 		free(args);
 		free(cptr);
-		free_module(module);
+		module_free(module);
 
 		return NULL;
 	}
@@ -391,7 +391,7 @@ static fmodule *do_execute_module(
 		free(arg1);
 		free(cptr);
 		free(args);
-		free_module(module);
+		module_free(module);
 
 		return NULL;
 	}
@@ -404,7 +404,7 @@ static fmodule *do_execute_module(
 		close(fvwm_to_app[0]);
 		close(fvwm_to_app[1]);
 		free(args);
-		free_module(module);
+		module_free(module);
 
 		return NULL;
 	}
