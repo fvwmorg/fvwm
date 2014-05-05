bb1cb12577c14b022d1e1833b6d9287994d72cfe
Thomas Adam <thomas@xteddy.org>
2014-05-05 11:02:45 +0100

    Include strlcpy in the source tree

--- libs/safemalloc.c
+++ libs/safemalloc.c
@@ -1,22 +1,21 @@
-/* This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+/* Memory management routines...
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see: <http://www.gnu.org/licenses/>
  */
+
+/* Originally from OpenBSD */
 
 #include "config.h"
 
 #include <stdio.h>
 #include <err.h>
 #include <sys/param.h>
+#include <stdint.h>
 #include "safemalloc.h"
 
 void *
