7e81fbfd19a613bc255dde819ada0c6ac3cb0b31
Dominik Vogt <dominik.vogt@gmx.de>
2014-09-12 23:23:02 +0100

    missing files

---
+++ mvwm/functable_complex.h
@@ -0,0 +1,53 @@
+/* -*-c-*- */
+
+#ifndef FUNCTABLE_COMPLEX_H
+#define FUNCTABLE_COMPLEX_H
+
+/* ---------------------------- included header files ---------------------- */
+
+/* ---------------------------- global definitions ------------------------- */
+
+/* ---------------------------- global macros ------------------------------ */
+
+/* ---------------------------- type definitions --------------------------- */
+
+typedef struct FunctionItem
+{
+	struct MvwmFunction *func;       /* the function this item is in */
+	struct FunctionItem *next_item;  /* next function item */
+	char condition;                  /* the character string displayed on
+					  * left*/
+	char *action;                    /* action to be performed */
+	short type;                      /* type of built in function */
+	func_flags_t flags;
+} FunctionItem;
+
+typedef struct MvwmFunction
+{
+	struct MvwmFunction *next_func;  /* next in list of root menus */
+	FunctionItem *first_item;        /* first item in function */
+	FunctionItem *last_item;         /* last item in function */
+	char *name;                      /* function name */
+	int use_depth;
+} MvwmFunction;
+
+/* Types of events for the FUNCTION builtin */
+typedef enum
+{
+	CF_IMMEDIATE =      'i',
+	CF_MOTION =         'm',
+	CF_HOLD =           'h',
+	CF_CLICK =          'c',
+	CF_DOUBLE_CLICK =   'd',
+	CF_TIMEOUT =        '-'
+} cfunc_action_t;
+
+/* ---------------------------- exported variables (globals) --------------- */
+
+/* ---------------------------- interface functions ------------------------ */
+
+MvwmFunction *NewMvwmFunction(const char *name);
+void DestroyFunction(MvwmFunction *func);
+Bool functions_is_complex_function(const char *function_name);
+
+#endif /* FUNCTABLE_COMPLEX_H */
