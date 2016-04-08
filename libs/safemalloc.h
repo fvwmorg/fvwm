6a53f113032c5099bb421e422612ced400200f74
Thomas Adam <thomas@xteddy.org>
2016-04-08 03:03:08 +0100

    compat: add asprintf if missing
    asprintf is not POSIX C, but is a GNU and BSD extension.  Although we already
    define _GNU_SOURCE, provide our own copy of asprintf in csae it's not on some
    systems.

--- libs/safemalloc.h
+++ libs/safemalloc.h
@@ -7,6 +7,12 @@
  *                   problem
  *
  */
+
+#ifndef HAVE_ASPRINTF
+int		 asprintf(char **, const char *, ...);
+int		 vasprintf(char **, const char *, va_list);
+#endif
+
 void *safemalloc(size_t);
 void *safecalloc(size_t, size_t);
 void *saferealloc(void *, size_t);
