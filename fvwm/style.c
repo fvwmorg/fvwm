cfd574884b3d0820d8f913da30feddf42398a04a
Thomas Adam <thomas@fvwm.org>
2016-06-24 00:24:17 +0100

    Multiborder colorsets
    WIP; from a really old patch.  Needs rework!

--- fvwm/style.c
+++ fvwm/style.c
@@ -2406,7 +2406,6 @@ static Bool style_parse_one_style_option(
 			ps->change_mask.use_border_colorset_handles_se = 1;
 
 			ps->change_mask.use_border_colorset_handles = 1;
-
                 }
 
 		else if (StrEquals(token, "BottomTitleRotated"))
@@ -3102,7 +3101,6 @@ static Bool style_parse_one_style_option(
 			ps->change_mask.use_border_colorset_hi_west = 1;
 
 			ps->change_mask.use_border_colorset_hi_regions = 1;
-
                 }
                 else if (StrEquals(token, "HilightHandlesColorsetRegions"))
                 {
@@ -3159,7 +3157,6 @@ static Bool style_parse_one_style_option(
 			ps->change_mask.use_border_colorset_hi_handles_se = 1;
 
 			ps->change_mask.use_border_colorset_hi_handles = 1;
-
                 }
                 else if (StrEquals(token, "HilightIconTitleColorset"))
 		{
