ThomasAdam/mvwm

Moved some functions to functable.[ch] and functable_complex.[ch].
This separation is needed for the parsing hooks.  It was very difficult to get
the #icludes right.  The DesktopsInfo structure was moved to libs/FScreen.h
so that the library does not depend on core header files anymore.

mvwm/misc.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <X11/Xatom.h>
+#include <X11/Xutil.h>
 
 #include "libs/ftime.h"
 #ifdef MVWM_DEBUG_TIME
@@ -36,6 +37,7 @@
 #endif
 #include "libs/Parse.h"
 #include "libs/Target.h"
+#include "libs/mvwmrect.h"
 #include "libs/FScreen.h"
 #include "mvwm.h"
 #include "externs.h"

fvwmorg/fvwm

fvwm/misc.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <X11/Xatom.h>
+#include <X11/Xutil.h>
 
 #include "libs/ftime.h"
 #ifdef FVWM_DEBUG_TIME
@@ -36,6 +37,8 @@
 #endif
 #include "libs/Parse.h"
 #include "libs/Target.h"
+#include "libs/fvwmrect.h"
+#include "libs/FScreen.h"
 #include "fvwm.h"
 #include "externs.h"
 #include "execcontext.h"